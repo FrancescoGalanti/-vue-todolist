56f3f84e356186ffe057cd86ea6073ad2c11b64e
FrancescoGalanti <galanti_francesco>
2020-11-10 14:36:59 +0100

    working

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -11,21 +11,25 @@
    data: {
      // To-do list array
       items:["doing grocery-shop", "washing the car","do the laundry"]
-
    },
    // Methods //
    methods:{
      // Call back function that add a new item in the list //
      additem(){
-       this.items.push(this.newitem)
+       // pushing new todo list in the array //
+       this.items.push(this.newitem.trim())
+       // clearing the input //
        this.newitem = "";
      },
-     // call back function that eliminate the item in the list
+     // call back function that eliminate the item in the list //
      removeitem(array,index){
        Vue.delete(this.items, index);
+
      }
 
    }
 
 
- })
+ });
+
+ console.log(this.items.length)
